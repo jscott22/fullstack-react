1a90aa1703c745a7a696be55f8a975d13e4979c7
Jason Scott <dev.jason.scott@gmail.com>
2017-08-03 12:13:29 +0200

    server refactor

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('./services/passport');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+require('./routes/authRoutes')(app);
+
+app.listen(PORT);

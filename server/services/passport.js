ae83e33c3fc3475096581d8bf455fb39ea3e405c
Jason Scott <dev.jason.scott@gmail.com>
2017-08-05 08:56:40 +0200

    sets up react

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,7 +13,7 @@ passport.deserializeUser((id, done) => {
     User.findById(id)
         .then((user) => {
             done(null, user);
-    });
+        });
 });
 
 passport.use(
@@ -24,18 +24,15 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({googleId: profile.id});
 
-            User.findOne({ googleId: profile.id })
-                .then((existingUser) => {
-                    if (existingUser) {
-                        done(null, existingUser);
-                    } else {
-                        new User({ googleId: profile.id })
-                            .save()
-                            .then(user => done(null, user));
-                    }
-                });
+            if (existingUser) {
+                return done(null, existingUser);
+            }
+
+            const user = await new User({googleId: profile.id}).save();
+            done(null, user);
         }
     )
 );
